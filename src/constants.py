f454a0af53b2540028da1a227cb6e6f69c001a2c
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-06 11:44:29 +0100

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
 import os
 
-ROOT_DIR = os.path.abspath(os.pardir)
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TRACKS_DIR = os.path.join(ROOT_DIR, "tracks")
+MUSDB_DIR = os.path.join(TRACKS_DIR, "musdb18")
