7e8bfe1fda539eb312bd98d5e5d270eba1fa04a2
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-06 18:01:34 +0100

    Create dataset.py

---
+++ src/dataset/dataset.py
@@ -0,0 +1,40 @@
+"""
+This file implements the dataset class which should handle the creation of the dataset for training
+and testing.
+"""
+
+import numpy as np
+from src.dataset.mus_data_handler import MusDataHandler
+from src.audio_utils.audio_utils import zero_pad, center_crop
+from src import constants
+
+
+class DataSet:
+    def __init__(self):
+        handler = MusDataHandler()
+        self.songs, self.vocals = handler.X, handler.y
+        self.create_dataset()
+
+    def create_dataset(self):
+        X = []
+        y = []
+
+        for i in range(len(self.songs)):
+
+            if i > 10:
+                break
+
+            song = zero_pad(self.songs[i])
+            vocal = zero_pad(self.vocals[i])
+
+            l, r = 0, constants.N_SAMPLES_IN
+            step = constants.N_SAMPLES_OUT
+
+            while r <= song.shape[0]:
+                X.append(np.array(song[l:r]))
+                y.append(center_crop(vocal[l:r]))
+
+                l += step
+                r += step
+
+        return np.array(X), np.array(y)
