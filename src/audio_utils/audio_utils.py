a32b4378e13d69dee97478d72d5de6ae592673ce
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-06 14:45:04 +0100

    added stereo to mono and zero padding function

--- src/audio_utils/audio_utils.py
+++ src/audio_utils/audio_utils.py
@@ -16,3 +16,40 @@ def save_array_as_wave(array, path, samplerate=constants.SAMPLE_RATE):
     :return: None
     """
     sf.write(path, array, samplerate)
+
+
+def stereo_to_mono(audio_stereo):
+    """
+    Converts a stereo audio signal to mono by averaging the channels.
+    :param audio_stereo: A numpy array with shape (samples, 2) representing the stereo audio.
+    :return: A numpy array with shape (samples,) representing the mono audio.
+    """
+    if audio_stereo.ndim != 2 or audio_stereo.shape[1] != 2:
+        raise ValueError("Input audio must be a stereo signal")
+
+    audio_mono = np.mean(audio_stereo, axis=1)
+
+    return audio_mono
+
+
+def zero_pad(array):
+    """
+    Pads the input audio array with zeros both at the beginning and at the end in the time domain.
+    Works for both mono and stereo signals.
+
+    If the input array is mono (1D), zeros are added at both the beginning and the end.
+    If the input array is stereo (2D), zeros are added at both the beginning and the end of each channel.
+
+    :param array: Input audio array, which can be mono (1D) or stereo (2D).
+    :return: Padded audio array.
+    """
+    pad_width = (constants.N_SAMPLES_IN, constants.N_SAMPLES_IN)
+
+    if array.ndim == 1:
+        padded_array = np.pad(array, pad_width, mode='constant', constant_values=0)
+    elif array.ndim == 2:
+        padded_array = np.pad(array, (pad_width, (0, 0)), mode='constant', constant_values=0)
+    else:
+        raise ValueError("Input array must be either mono or stereo.")
+
+    return padded_array
