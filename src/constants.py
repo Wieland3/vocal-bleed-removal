e0d3fd33933d07412aa6b3c39c0222fd215eb0b2
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-06 13:37:02 +0100

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,10 @@
 import os
 
+# Folders
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TRACKS_DIR = os.path.join(ROOT_DIR, "tracks")
 MUSDB_DIR = os.path.join(TRACKS_DIR, "musdb18")
+NPZ_DATA_DIR = os.path.join(ROOT_DIR, "npz_data")
+
+# Files
+PATH_TO_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "musdb18.npz")
