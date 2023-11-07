212764380d7b9ded44f9cdab953f40ae6f49ffc8
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-07 14:26:12 +0100

    added path to wave unet

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,7 @@ TRACKS_DIR = os.path.join(ROOT_DIR, "tracks")
 MUSDB_DIR = os.path.join(TRACKS_DIR, "musdb18")
 NPZ_DATA_DIR = os.path.join(ROOT_DIR, "npz_data")
 DEBUGGING_DATA_DIR = os.path.join(TRACKS_DIR, "debugging")
+WAVE_UNET = os.path.join(ROOT_DIR, "wave-unet")
 
 # Files
 PATH_TO_RAW_TRAIN_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "raw_train_musdb18.npz")
