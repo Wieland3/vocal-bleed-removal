1ca5b338bc09796c64f84d5f46425f69c4c49b25
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-09 17:23:17 +0100

    remove unneeded params from predict.py

--- src/train/predict.py
+++ src/train/predict.py
@@ -9,26 +9,10 @@ import sys
 sys.path.insert(0, constants.WAVE_UNET)
 from wave_u_net import wave_u_net
 
-params = {
-  "num_initial_filters": 12,
-  "num_layers": 12,
-  "kernel_size": 15,
-  "merge_filter_size": 5,
-  "source_names": ["vocals"],
-  "num_channels": 2,
-  "output_filter_size": 1,
-  "padding": "valid",
-  "input_size": 147443,
-  "context": True,
-  "upsampling_type": "learned",         # "learned" or "linear"
-  "output_activation": "linear",        # "linear" or "tanh"
-  "output_type": "direct",          # "direct" or "difference"
-}
-
 if __name__ == "__main__":
     test = dataset.DataSet(subsets="test")
 
-    checkpoint_path = constants.CHECKPOINTS_DIR + "/best_model/cp.ckpt"
+    checkpoint_path = constants.CHECKPOINTS_DIR + "/best_model_2/cp.ckpt"
     model = tf.keras.models.load_model(checkpoint_path)
 
     """
