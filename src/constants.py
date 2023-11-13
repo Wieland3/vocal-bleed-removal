2aaddeec2357f636019c50eb11b8a073afdfab6b
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-11-13 14:43:30 +0100

    edited constants for artifical datasets

--- src/constants.py
+++ src/constants.py
@@ -14,8 +14,8 @@ EDA_DATA_DIR = os.path.join(TRACKS_DIR, "eda")
 # Files
 PATH_TO_RAW_TRAIN_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "raw_train_musdb18.npz")
 PATH_TO_RAW_TEST_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "raw_test_musdb18.npz")
-PATH_TO_EDIT_TRAIN_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "edit_train_musdb18.npz")
-PATH_TO_EDIT_TEST_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "edit_test_musdb18.npz")
+PATH_TO_ART_TRAIN_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "art_train_musdb18.npz")
+PATH_TO_ART_TEST_MUSDB_NPZ = os.path.join(NPZ_DATA_DIR, "art_test_musdb18.npz")
 
 # Audio
 SAMPLE_RATE = 44100
@@ -23,6 +23,6 @@ N_SAMPLES_IN = 147443
 N_SAMPLES_OUT = 16389
 
 # SONGS
-VALID_FEMALE_VOCS = [0, 11, 14, 18, 21, 29, 26, 35, 38, 37, 42, 44, 47, 49]
-TRAIN_FEMALE_VOCS = [4, 5, 11, 10, 7, 6, 17, 21, 20, 19, 18, 26, 32, 37, 47, 48, 49,
+VALID_FEMALE_VOCS = [11, 14, 18, 21, 26, 35, 38, 37, 42, 44, 47, 49]
+TRAIN_FEMALE_VOCS = [4, 5, 11, 10, 7, 6, 17, 21, 20, 19, 18, 26, 37, 47, 48, 49,
                      56, 58, 59, 60, 66, 69, 72, 74, 85, 88]
