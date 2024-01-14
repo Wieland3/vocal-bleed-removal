85b0d07babff10345c36e7c6c58609fd9025f608
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2024-01-14 12:07:04 +0100

    Update time_noise_gate.py

--- src/audio_utils/time_noise_gate.py
+++ src/audio_utils/time_noise_gate.py
@@ -1,12 +1,14 @@
-from src.audio_utils.noise_gate import NoiseGate
+from src.audio_utils.noise_gate import NoiseGateParent
 import numpy as np
+from pedalboard import Pedalboard, NoiseGate
+from src import constants
 
 
-class TimeNoiseGate(NoiseGate):
+class TimeNoiseGate(NoiseGateParent):
     def __init__(self, threshold):
         super().__init__(threshold)
+        self.board = Pedalboard([NoiseGate(threshold_db=threshold, release_ms=400)])
 
     def process(self, audio):
-        threshold = 10 ** (self.threshold / 20.0)
-        return np.where((np.abs(audio) > threshold), audio, 0)
+        return self.board(audio, sample_rate=constants.SAMPLE_RATE)
 
