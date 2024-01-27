98a5df102da89ce153fb9eeddbd10eb8e659d609
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2024-01-27 10:23:09 +0100

    Update metric.py

--- src/evaluation/metric.py
+++ src/evaluation/metric.py
@@ -15,15 +15,6 @@ def l1_loss_db(y_true, y_pred, n_fft=2048):
     return l1_loss_db
 
 
-def frequency_domain_loss(y_true, y_pred):
-    fft_y_true = tf.signal.fft(tf.cast(y_true[0], tf.complex64))
-    fft_y_pred = tf.signal.fft(tf.cast(y_pred[0], tf.complex64))
-
-    loss = tf.reduce_mean(tf.abs(tf.math.subtract(fft_y_true, fft_y_pred)) ** 2)
-
-    return loss
-
-
 def sdr(y_true, y_pred):
     delta = 1e-7
     numerator = np.sum(np.square(y_true))
@@ -35,12 +26,9 @@ def sdr(y_true, y_pred):
 
 def sdr_tf(y_true, y_pred):
     delta = 1e-7
-
     numerator = tf.math.reduce_sum(tf.math.square(y_true))
     denominator = tf.math.reduce_sum(tf.math.square(y_true - y_pred))
-
     numerator += delta
     denominator += delta
-
     return 10 * tf.experimental.numpy.log10(tf.math.divide(numerator, denominator))
 
